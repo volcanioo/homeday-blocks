52ec0bb19a7a3a6cc536e003e01f6d17b36643ef
Ilyas Amezouar <ilyas@amezouar.com>
2019-05-31 18:06:34 +0200

    :sparkle: Added HdSplitInput from MeinHomeday

--- src/components/form/HdDynamicForm.vue
+++ src/components/form/HdDynamicForm.vue
@@ -28,6 +28,7 @@ import HdPasswordConfirm from 'hd-blocks/components/form/HdPasswordConfirm.vue';
 import HdTextarea from 'hd-blocks/components/form/HdTextarea.vue';
 import HdSelect from 'hd-blocks/components/form/HdSelect.vue';
 import HdTagsSelector from 'hd-blocks/components/form/HdTagsSelector.vue';
+import HdSplitInput from 'hd-blocks/components/form/HdSplitInput.vue';
 import HdGoogleAutocomplete from 'hd-blocks/components/form/HdGoogleAutocomplete.vue';
 
 export default {
@@ -111,6 +112,8 @@ export default {
           return HdSelect;
         case 'tagsSelector':
           return HdTagsSelector;
+        case 'splitInput':
+          return HdSplitInput;
         case 'googleAutocomplete':
           return HdGoogleAutocomplete;
         default:
